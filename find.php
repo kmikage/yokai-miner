a8fc79997db717c682669ba0623b1073b7b4b29e
kei mikage <kmikage@gmail.com>
2021-12-25 19:31:55 +0900

    辞書・コンフィグファイルの適用

--- find.php
+++ find.php
@@ -7,13 +7,17 @@
 // Configration
 
 // パスワードの文字数
-$config['length']       = 14;
+$config['len']  = 14;
 // デバッグ表示
-$config['debug']        = false;
+$config['view'] = false;
 // チェックの処理をバックグラウンドで実行
-$config['background']   = true;
+$config['bg']   = true;
 
-// build ytoa array
+// コンフィグのロード
+require_once('./config.php');
+
+// 辞書の構築
+require_once($config['dicfile']);
 $ytoa=build_ytoa();
 
 // ---
@@ -35,10 +39,10 @@ function exec_chk($PASSWORD) {
     global $config;
 
     // for Debug.
-    if($config['debug']) echo ('Testing -> '. $PASSWORD .'('. strlen($PASSWORD) .')'. "\n");
+    if($config['view']) echo ('Testing -> '. $PASSWORD .'('. strlen($PASSWORD) .')'. "\n");
 
     // Kick checker
-    if($config['background']) {
+    if($config['bg']) {
         exec ('./chk.sh '.$PASSWORD. '&' ); // for background
     } else {
         exec ('./chk.sh '.$PASSWORD );} // for foreground
@@ -47,7 +51,7 @@ function exec_chk($PASSWORD) {
 function build_password($PASSWORD, $len){
     global $ytoa;
     global $config;
-    if (strlen($PASSWORD) == $config['length']) {
+    if (strlen($PASSWORD) == $config['len']) {
         exec_chk($PASSWORD);
     } else {
         $w=$ytoa[rand(0, count($ytoa) - 1)];
@@ -61,52 +65,4 @@ function build_password($PASSWORD, $len){
     }
     ***/
     }
-}
-
-
-function build_ytoa() {
-    $ytoa[]="A";
-    $ytoa[]="B";
-    $ytoa[]="C";
-    $ytoa[]="D";
-    $ytoa[]="E";
-    $ytoa[]="F";
-    $ytoa[]="G";
-    $ytoa[]="H";
-    $ytoa[]="I";
-    $ytoa[]="J";
-    $ytoa[]="K";
-    $ytoa[]="L";
-    $ytoa[]="M";
-    $ytoa[]="N";
-    $ytoa[]="O";
-    $ytoa[]="P";
-    $ytoa[]="Q";
-    $ytoa[]="R";
-    $ytoa[]="S";
-    $ytoa[]="T";
-    $ytoa[]="U";
-    $ytoa[]="V";
-    $ytoa[]="W";
-    $ytoa[]="X";
-    $ytoa[]="Y";
-    $ytoa[]="Z";
-    $ytoa[]="-";
-    $ytoa[]=".";
-    $ytoa[]="1";
-    $ytoa[]="2";
-    $ytoa[]="3";
-    $ytoa[]="4";
-    $ytoa[]="5";
-    $ytoa[]="6";
-    $ytoa[]="7";
-    $ytoa[]="8";
-    $ytoa[]="9";
-    $ytoa[]="0";
-    $ytoa[]="!";
-    $ytoa[]="n";
-    $ytoa[]="m";
-    $ytoa[]="c";
-
-    return($ytoa);
 }
